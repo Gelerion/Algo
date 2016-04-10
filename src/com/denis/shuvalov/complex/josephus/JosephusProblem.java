8a4640a00dbe6a606b7df417e67f17c35f72abe2
Denis Shuvalov <nip.tresk@gmail.com>
2016-04-10 18:21:21 +0300

    josephus problem

---
+++ src/com/denis/shuvalov/complex/josephus/JosephusProblem.java
@@ -0,0 +1,11 @@
+package com.denis.shuvalov.complex.josephus;
+
+import java.util.Scanner;
+
+public class JosephusProblem {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter amount of people at the circle");
+    }
+}

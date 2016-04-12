b0d3014f994eb663ee5d7edef2c3aa6a91e7197d
Denis Shuvalov <nip.tresk@gmail.com>
2016-04-12 18:19:26 +0300

    recursion: anagrams

---
+++ src/com/denis/shuvalov/algo/recursion/anagrams/Anagrams.java
@@ -0,0 +1,40 @@
+package com.denis.shuvalov.algo.recursion.anagrams;
+
+/**
+ * Перестановкой называется расположение объектов в определенном
+ * порядке. Предположим, требуется составить полный список анаграмм заданного
+ * слова, то есть всех возможных перестановок букв (независимо от того, являются
+ * они допустимыми словами или нет). Например, для слова cat программа должна
+ * вывести следующий список анаграмм:
+ * cat
+ * cta
+ * atc
+ * act
+ * tca
+ * tac
+ * количество вариантов равно факториалу количества букв
+ */
+public class Anagrams {
+
+    public static void main(String[] args) {
+        loopAnagram("cats");
+    }
+
+    static void loopAnagram(String word) {
+        if (word.isEmpty()) return;
+
+        int counter = 1;
+        for (int i = 0; i < word.length(); i++) {
+            char currentFirst = word.charAt(i);
+            String tmp = word.substring(0, i) + word.substring(i + 1);
+            if (counter % 10 == 0) System.out.println();
+            System.out.print(counter++ + ":" + currentFirst + tmp + "\t");
+
+            for (int j = 0; j < tmp.length(); j++) {
+                tmp = tmp.substring(1) + tmp.substring(0, 1);
+                if (counter % 10 == 0) System.out.println();
+                System.out.print(counter++ + ":" + currentFirst + tmp + "\t");
+            }
+        }
+    }
+}
---
+++ src/com/denis/shuvalov/algo/recursion/sample/SimpleRecursionApp.java
@@ -0,0 +1,62 @@
+package com.denis.shuvalov.algo.recursion.sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SimpleRecursionApp {
+    static int theNumber;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a number: ");
+        theNumber = getInt();
+        int theAnswer = triangle(theNumber);
+        System.out.println("Triangle: " + theAnswer);
+        theAnswer = factorial(theNumber);
+        System.out.println("Factorial: " + theAnswer);
+    }
+
+    //sum of all numbers
+    //n-е треугольное число = (n2+n)/2
+    //4 + 3 + 2 + 1 дает 10.
+    private static int triangle(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        else return n + triangle(n - 1);
+    }
+
+/*    public static int triangle(int n)
+    {
+        System.out.println("Entering: n=" + n);
+        if(n==1)
+        {
+            System.out.println("Returning 1");
+            return 1;
+        }
+        else
+        {
+            int temp = n + triangle(n-1);
+            System.out.println("Returning " + temp);
+            return temp;
+        }
+    }*/
+
+    //факториал 5 вычисляется по формуле
+    //5 × 4 × 3 × 2 × 1 = 120.
+    private static int factorial(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        else return n * factorial(n - 1);
+    }
+
+    private static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+
+    private static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
+    }
+}

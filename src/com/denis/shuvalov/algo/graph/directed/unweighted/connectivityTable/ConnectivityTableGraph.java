be7537fdff4c9462f5a3a27afa9c49320e598aa7
Denis Shuvalov <denis.shuvalov1@gmail.com>
2017-06-26 16:39:27 +0300

    -  Warshall + mst + shortest path algos

--- src/com/denis/shuvalov/algo/graph/directed/unweighted/connectivityTable/ConnectivityTableGraph.java
+++ src/com/denis/shuvalov/algo/graph/directed/unweighted/connectivityTable/ConnectivityTableGraph.java
@@ -1,22 +1,14 @@
 package com.denis.shuvalov.algo.graph.directed.unweighted.connectivityTable;
 
+import java.util.Stack;
+
 /**
- * Программу dfs  легко изме-
- * нить таким образом, чтобы обход последовательно
- * начинался с каждой вершины. Вот как выглядит
- * результат ее выполнения для графа на рис. 13.15:
- * AC
- * BACE
- * C
- * DEC
- * EC
- * Мы получили таблицу связности для направ-
- * Рис. 13.15. Направленный
- * ленного графа. Первая буква в строке определяет
- * граф
- * начальную вершину, а остальные буквы — вершины,
- * к которым можно перейти (напрямую или через другие вершины) от начальной
- * вершины.
+ * Программу dfs легко изме- нить таким образом, чтобы обход последовательно
+ * начинался с каждой вершины. Вот как выглядит результат ее выполнения для
+ * графа: AC BACE C DEC EC Мы получили таблицу связности для направ- ленного
+ * графа. Первая буква в строке определяет граф начальную вершину, а остальные
+ * буквы — вершины, к которым можно перейти (напрямую или через другие вершины)
+ * от начальной вершины.
  */
 public class ConnectivityTableGraph {
     private final int MAX_VERTS = 20;
@@ -28,6 +20,44 @@ public class ConnectivityTableGraph {
         this.vertexList = new Vertex[MAX_VERTS];
         this.adjMatrix = new int[MAX_VERTS][MAX_VERTS];
     }
+
+	void connectTable() {
+		for (int i = 0; i < nVerts; i++) {
+			System.out.print(vertexList[i] + " ");
+			vertexList[i].wasVisited = true;
+
+			Stack<Integer> stack = new Stack<>();
+			stack.add(i);
+
+			while (!stack.isEmpty()) {
+				Integer curr = stack.peek();
+				int next = getNext(curr);
+
+				if (next == -1) {
+					stack.pop();
+				} else {
+					stack.add(next);
+					System.out.print(vertexList[next] + " ");
+					vertexList[next].wasVisited = true;
+				}
+			}
+
+			System.out.println();
+			for (int j = 0; j < nVerts; j++) {
+				vertexList[j].wasVisited = false;
+			}
+		}
+	}
+
+	private int getNext(int index) {
+		for (int i = 0; i < adjMatrix[index].length; i++) {
+			if (adjMatrix[index][i] == 1 && !vertexList[i].wasVisited) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
 
     void addVertx(char label) {
         vertexList[nVerts++] = new Vertex(label);
---
+++ src/com/denis/shuvalov/algo/graph/undirected/weighted/shortestPath/TestShortest.java
@@ -0,0 +1,24 @@
+package com.denis.shuvalov.algo.graph.undirected.weighted.shortestPath;
+
+public class TestShortest {
+	public static void main(String[] args) {
+		ShortestPathSelectionGrpah grpah = new ShortestPathSelectionGrpah();
+
+		grpah.addVertex('A'); // 0
+		grpah.addVertex('B'); // 1
+		grpah.addVertex('C'); // 2
+		grpah.addVertex('D'); // 3
+		grpah.addVertex('E'); // 4
+
+		grpah.addEdge(0, 1, 50); // AB 50
+		grpah.addEdge(0, 3, 80); // AD 80
+		grpah.addEdge(1, 3, 90); // BD 90
+		grpah.addEdge(1, 2, 60); // BC 60
+		grpah.addEdge(2, 4, 40); // CE 40
+		grpah.addEdge(3, 2, 20); // DC 20
+		grpah.addEdge(3, 4, 70); // DE 70
+		grpah.addEdge(4, 1, 50); // EB 50
+
+		grpah.shortestPath();
+	}
+}
